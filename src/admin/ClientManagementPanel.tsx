6940a126248884019f5dcdcaa8a6934f4ae4c04f
amenagementmonzon <silviolmonzon@amenagementmonzon.com>
2026-03-18 21:55:59 -0400

    client
    client

--- src/admin/ClientManagementPanel.tsx
+++ src/admin/ClientManagementPanel.tsx
@@ -1,24 +1,23 @@
 import { useState } from "react";
 import { Plus } from "@phosphor-icons/react";
 
-const clients = [];
-const isPending = false;
-
-const create = async () => {};
-const update = async () => {};
-const remove = async () => {};
-const isMutating = false;
-
 export default function ClientManagementPanel() {
+  const clients = [];
+  const isPending = false;
+
+  const create = async () => {};
+  const update = async () => {};
+  const remove = async () => {};
+  const isMutating = false;
+
   return (
     <div>
       <h1>Client Management</h1>
-      <button>+ Add Client</button>
+      <button>Add Client</button>
       <div>
         {clients.map((client) => (
           <div key={client.id}>
             <h3>{client.name}</h3>
-            <button onClick={() => remove(client.id)}>Delete</button>
           </div>
         ))}
       </div>
